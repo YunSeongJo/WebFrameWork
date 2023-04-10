326c3b372e2a654eb3af30dbe0187023a2207db2
YunSeongJo <jys991@naver.com>
2023-04-10 18:38:17 +0900

    04_10

---
+++ WebProject/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view()),
+    # path('', views.index),  # server/blog/
+    path('<int:pk>/', views.single_post_page),  # server/blog/1/
+
+]
--- WebProject/blog/views.py
+++ WebProject/blog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+# class 쓰면 내부적으로 기능을 사용하고, 간단해지는게 장점
+class PostList(ListView):  # 모델명_list.html 찾도록 강제로 세팅되어있음..따라서 index.html 를 post.html 로 바꾸거나 templates 이름을 변경하면됌
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/post_list.html'
+
+
+# def index(request) :
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request, 'blog/post_list.html',
+#        {
+#            'posts': posts,
+#        }
+#    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request, 'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
